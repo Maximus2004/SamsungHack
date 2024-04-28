2f08759ccb4311fd64922c24d06d75d30aa96948
Minuano2653 <artemlikh2004@gmail.com>
2024-04-28 09:55:39 +0300

    Moy Commit

--- app/src/main/java/com/example/samsunghackaton/TestActivity.kt
+++ app/src/main/java/com/example/samsunghackaton/TestActivity.kt
@@ -2,10 +2,13 @@ package com.example.samsunghackaton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.fragment_sign_up_or_enter)
+
+        val button = findViewById<Button>()
     }
 }
